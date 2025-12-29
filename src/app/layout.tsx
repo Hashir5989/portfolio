96cd0318cdf8472bc2893171e1993980812d1374
hashir5989 <hashirbinali@gmail.com>
2025-12-29 12:51:28 +0000

    Fix layout TypeScript errors by removing authentication props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,35 +7,30 @@ import ScrollToTop from '@/components/ScrollToTop';
 import Aoscompo from "@/utils/aos";
 import NextTopLoader from 'nextjs-toploader';
 // import SessionProviderComp from "@/components/nextauth/SessionProvider";
-import { AuthDialogProvider } from "./context/AuthDialogContext";
 const inter = Inter({ subsets: ["latin"] });
 
 
 export default function RootLayout({
   children,
-  session,
 }: Readonly<{
   children: React.ReactNode;
-  session:any
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
       <NextTopLoader />
-      <AuthDialogProvider>
-        <ThemeProvider
-          attribute="class"
-          enableSystem={true}
-          defaultTheme="system"
-        >
-          <Aoscompo>
-            <Header />
-            {children}
-            <Footer />
-          </Aoscompo>
-          <ScrollToTop />
-        </ThemeProvider>
-      </AuthDialogProvider>
+      <ThemeProvider
+        attribute="class"
+        enableSystem={true}
+        defaultTheme="system"
+      >
+        <Aoscompo>
+          <Header />
+          {children}
+          <Footer />
+        </Aoscompo>
+        <ScrollToTop />
+      </ThemeProvider>
       </body>
     </html>
   );
